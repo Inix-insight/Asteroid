61cbec283e3885dbdaa2e4918f79139c0010fcbc
Inix-insight <jasonlouhe@hotmail.com>
2026-01-21 23:18:20 +0800

    chapter 4-1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -9,15 +11,20 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    
+    asteroids = pygame.sprite.Group()
+    asteroidfield = pygame.sprite.Group()
+
     dt = 0
+
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#    print(f"Starting Asteroids with pygame version: {pygame.version.ver }")
- #   print(f"Screen width: {SCREEN_WIDTH}")
-  #  print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         log_state()
         for event in pygame.event.get():
